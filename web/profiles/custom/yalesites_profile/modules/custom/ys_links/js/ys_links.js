f752ee1e2d99cec1e996dee904a99f9ea1eebd5c
David Blankenship <david.blankenship@yale.edu>
2023-10-30 11:29:30 -0400

    feat(YALB-520): allow settings override
    To ensure we have flexibility of the items we want to target, we allow
    the following items to be changed during runtime:
        - debug: Whether to show debug information (default: false)
        - excludedClasses: Array of link classes to not capture (default:
          [])
        - contextStart: Within the context (currently page), what selector
          should be used as the starting point of finding links.

--- web/profiles/custom/yalesites_profile/modules/custom/ys_links/js/ys_links.js
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_links/js/ys_links.js
@@ -8,16 +8,32 @@
     'use strict';
 
     Drupal.ys_links = Drupal.ys_links || {};
-    Drupal.ys_links.debugging = true;
+    Drupal.ys_links.debugging = false;
 
-    Drupal.ys_links.attach = function(context, _drupalSettings) {
-        const pageContext = context.querySelector('#main-content');
-        const excludedClasses = [
-            '.site-header__yale-branding',  // Yale header logo
-            '.site-footer__site-branding',  // Yale footer logo
-        ].join(', ');
+    const getExclusionParams = (excludedClasses) => {
+        if (excludedClasses.length > 0) {
+            return `a:not(${excludedClasses})`;
+        }
 
-        const links = pageContext.querySelectorAll(`a:not(${excludedClasses})`);
+        return 'a';
+    };
+
+    Drupal.ys_links.attach = function(context, drupalSettings) {
+        const defaultDrupalSettings = {
+            contextStart: '#main-content',
+            excludedClasses: [],
+            debug: false,
+        };
+
+        drupalSettings.ys_links = drupalSettings.ys_links || defaultDrupalSettings;
+        drupalSettings.ys_links = Object.assign(defaultDrupalSettings, drupalSettings.ys_links);
+        Drupal.ys_links.debugging = drupalSettings.ys_links.debug;
+
+        const contextStart = drupalSettings.ys_links.contextStart;
+        const pageContext = context.querySelector(contextStart);
+        const excludedClasses = drupalSettings.ys_links.excludedClasses.join(', ');
+
+        const links = pageContext.querySelectorAll(getExclusionParams(excludedClasses));
 
         links.forEach(function(link) {
             var linkRenderer = Drupal.ys_links.getLinkRenderer(link);
