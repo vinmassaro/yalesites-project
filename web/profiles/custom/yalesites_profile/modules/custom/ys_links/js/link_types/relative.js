1ac3137d437babc223367f13e105cbbeb0ef9275
David Blankenship <david.blankenship@yale.edu>
2023-11-07 12:46:13 -0500

    fix(YALB-520): add treatment for internal links
    I also noticed that there was a link "Add content" generated by Drupal
    that was not being properly styled.  This now styles it and any others.

--- web/profiles/custom/yalesites_profile/modules/custom/ys_links/js/link_types/relative.js
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_links/js/link_types/relative.js
@@ -14,6 +14,17 @@
         // eslint-disable-next-line no-console
         console.log(`${link.getAttribute("href")} is relative`);
       }
+
+      link.classList.add("ys_linked");
+      link.classList.add("link");
+
+      if (!link.dataset.linkStyle) {
+        link.dataset.linkStyle = "underline";
+      }
+
+      if (!link.dataset.linkType) {
+        link.dataset.linkType = "normal";
+      }
     },
   };
 })(Drupal);
