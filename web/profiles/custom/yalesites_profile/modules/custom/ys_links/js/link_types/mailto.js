6ca2cab7cfebbc2d0e22f0c5e30baecadefecf51
David Blankenship <david.blankenship@yale.edu>
2023-12-13 08:48:26 -0500

    chore(YALB-520): remove unused code
    This might be used in the future, but for now we are bypassing mailto
    and telephone decorations.

--- web/profiles/custom/yalesites_profile/modules/custom/ys_links/js/link_types/mailto.js
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_links/js/link_types/mailto.js
@@ -7,30 +7,6 @@
   Drupal.ys_links = Drupal.ys_links || {};
   Drupal.ys_links.linkTypes = Drupal.ys_links.linkTypes || {};
 
-  const sanitizeEmailAddress = (emailLink) => {
-    let link = emailLink.replace(/^mailto:/i, "");
-
-    if (link.includes("?")) {
-      [link] = link.split("?");
-    }
-
-    return link;
-  };
-
-  const createMailtoLinkHrefSpan = (link) => {
-    const span = document.createElement("span");
-    span.classList.add("pre-text__text", "visually-hidden");
-    span.setAttribute("aria-hidden", true);
-    span.innerHTML = sanitizeEmailAddress(link.getAttribute("href"));
-    return span;
-  };
-
-  // Moving the button inside the span so things like grand hero will behave
-  const renderCopyInsideOfElement = (element) => {
-    element.innerHTML += " ";
-    element.appendChild(Drupal.ys_links.createCopyButtonWithIcon());
-  };
-
   const isMailToLink = (link) => {
     return link.getAttribute("href").startsWith("mailto:");
   };
@@ -45,26 +21,7 @@
         return;
       }
 
-      // Remove these two lines when we're ready to actually use.
       link.classList.add("ys_skipped");
-      return;
-
-      if (!navigator.clipboard) {
-        link.classList.add("ys_skipped", "ys_linked", "link--with-icon");
-        return;
-      }
-
-      const clonedLink = link.cloneNode(true);
-      clonedLink.classList.add("ys_mailto", "ys_linked", "link--with-icon");
-
-      clonedLink.appendChild(document.createTextNode(" "));
-      renderCopyInsideOfElement(clonedLink);
-      clonedLink.appendChild(createMailtoLinkHrefSpan(link));
-
-      link.insertAdjacentElement("afterend", clonedLink);
-      link.remove();
-
-      Drupal.ys_links.debugLog(`${link.getAttribute("href")} is mailto`);
     },
   };
 })(Drupal);
--- web/profiles/custom/yalesites_profile/modules/custom/ys_links/js/link_types/telephone.js
+++ web/profiles/custom/yalesites_profile/modules/custom/ys_links/js/link_types/telephone.js
@@ -7,30 +7,8 @@
   Drupal.ys_links = Drupal.ys_links || {};
   Drupal.ys_links.linkTypes = Drupal.ys_links.linkTypes || {};
 
-  const createTelephoneLinkHrefSpan = (link) => {
-    const span = document.createElement("span");
-    span.classList.add("pre-text__text");
-    span.classList.add("visually-hidden");
-    span.setAttribute("aria-hidden", true);
-    span.innerHTML = link.getAttribute("href");
-    return span;
-  };
-
-  const prependProtocol = (link) => {
-    if (link.getAttribute("href").match(/^tel:/)) {
-      return link.getAttribute("href");
-    }
-    return `tel:${link.getAttribute("href")}`;
-  };
-
   const TELEPHONE_REGEX =
     /^(?:(?:tel:)?(?:(?:\+|00)\d{1,3}\s?)?[ -.()]*\d{1,4}[ -.()]*\d{1,4}[ -.()]*\d{1,4}[ -.()]*\d{1,4})$/;
-
-  // Moving the button inside the span so things like grand hero will behave
-  const renderCopyInsideOfElement = (element) => {
-    element.innerHTML += " ";
-    element.appendChild(Drupal.ys_links.createCopyButtonWithIcon());
-  };
 
   Drupal.ys_links.linkTypes.telephone = {
     weight: 20,
@@ -59,25 +37,10 @@
         );
       }
 
-      // Remove this block when ready to use
       if (!hasTel) {
         link.setAttribute("href", `tel:${link.getAttribute("href")}`);
       }
       link.classList.add("ys_skipped");
-      return;
-
-      const clonedLink = link.cloneNode(true);
-      clonedLink.classList.add("ys_telephone", "ys_linked", "link--with-icon");
-      clonedLink.setAttribute("href", prependProtocol(clonedLink));
-
-      clonedLink.appendChild(document.createTextNode(" "));
-      renderCopyInsideOfElement(clonedLink);
-      clonedLink.appendChild(createTelephoneLinkHrefSpan(link));
-
-      link.insertAdjacentElement("afterend", clonedLink);
-      link.remove();
-
-      Drupal.ys_links.debugLog(`${link.getAttribute("href")} is a telephone`);
     },
   };
 })(Drupal, drupalSettings);
